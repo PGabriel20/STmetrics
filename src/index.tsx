639fd94c137acf367611484d1ba0bc6037522ca7
PGabriel20 <bielporreca@gmail.com>
2021-08-11 22:36:55 -0300

    fix:  injecting app into layout component

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Layout from './components/Layout/Layout';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Layout>
+      <App />
+    </Layout>
   </React.StrictMode>,
   document.getElementById('root')
 );
