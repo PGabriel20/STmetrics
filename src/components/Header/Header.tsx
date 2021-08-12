e49e33e4c1f91f53e0d952901e85159fc7876a17
PGabriel20 <bielporreca@gmail.com>
2021-08-11 22:01:31 -0300

    feat: styled header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,14 @@ import React from 'react';
 import './styles.scss';
 
 const Header: React.FC = () => {
-  return <div />;
+  return (
+    <header>
+      <div className="innerContainer">
+        <h1><span>SW</span>metrics</h1>
+        <a href="#">API LINK</a>
+      </div>
+    </header>  
+  );
 }
 
 export default Header;
