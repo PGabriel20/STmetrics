ec6f17b2e6b0b64bc0cce048dba715cba5dc3ffb
PGabriel20 <bielporreca@gmail.com>
2021-08-13 18:11:26 -0300

    fix: making multiple requests

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -13,28 +13,39 @@ import api from '../../services/api';
 import './styles.scss';
 
 const LandingPage: React.FC = () => {
-  const [peopleCount, setPeopleCount] = useState([]);
-  const [planetCount, setPlanetCount] = useState([]);
-  const [starshipCount, setStarshipCount] = useState([]);
-  const [vehicleCount, setVehicleCount] = useState([]);
+  const [peopleCount, setPeopleCount] = useState(0);
+  const [planetCount, setPlanetCount] = useState(0);
+  const [starshipCount, setStarshipCount] = useState(0);
+  const [vehicleCount, setVehicleCount] = useState(0);
+  const [movieCount, setMovieCount] = useState(0);
 
 
-  // async function fetchMainData() {
-  //   const apiURLs = ['/people', '/planets', '/starships', '/vehicles'];
+  async function fetchMainData() {
+    const apiURLs = ['/people', '/planets', '/starships', '/vehicles', '/films'];
 
-  //   const getPeople = await api.get(apiURLs[0]);
-  //   const getVehicles = await api.get(apiURLs[1]);
+    const getPeople = await api.get(apiURLs[0]);
+    const getPlanets = await api.get(apiURLs[1]);
+    const getStarships = await api.get(apiURLs[2]);
+    const getVehicles = await api.get(apiURLs[3]);
+    const getMovies = await api.get(apiURLs[4]);
 
-  //   axios.all([getPeople, getVehicles]).then(
-  //     axios.spread((...allData) => {
-  //       const peopleCount = allData[0].data.count;
-  //       const vehiclesArray = allData[1].data.count;
+    axios.all([getPeople, getPlanets, getStarships, getVehicles, getMovies]).then(
+      axios.spread((...allData) => {
+        const peopleAmount = allData[0].data.count;
+        const planetAmount = allData[1].data.count;
+        const starshipAmount = allData[2].data.count;
+        const vehicleAmount = allData[3].data.count;
+        const movieAmount = allData[4].data.count;
 
-  //     })
-  //   )
-  // }
+        setPeopleCount(peopleAmount);
+        setPlanetCount(planetAmount);
+        setStarshipCount(starshipAmount);
+        setVehicleCount(vehicleAmount);
+        setMovieCount(movieAmount);
 
-  const url = "/vehicles"
+      })
+    )
+  }
 
   async function downloadPeopleData() {
 
@@ -82,6 +93,7 @@ const LandingPage: React.FC = () => {
 
   useEffect(() => {
     downloadPeopleData();
+    fetchMainData();
   }, []);
 
   return (
@@ -91,11 +103,11 @@ const LandingPage: React.FC = () => {
         <h2>STAR WARS - Metrics and statistics</h2>
       </section>
       <div className='topCards'>
-        <People />
-        <Planets />
-        <Starships />
-        <Vehicles />
-        <Movies />
+        <People amount={peopleCount}/>
+        <Planets amount={planetCount}/>
+        <Starships amount={starshipCount}/>
+        <Vehicles amount={vehicleCount}/>
+        <Movies amount={movieCount}/>
       </div>
 
       <div className='bottomCards'>
