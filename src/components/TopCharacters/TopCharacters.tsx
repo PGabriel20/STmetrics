e9f032e030715af02ae9285e58b25f9343ebaa46
PGabriel20 <bielporreca@gmail.com>
2021-08-14 15:54:42 -0300

    feat: fetching and sorting film data

--- src/components/TopCharacters/TopCharacters.tsx
+++ src/components/TopCharacters/TopCharacters.tsx
@@ -1,16 +1,85 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
 const TopCharacters: React.FC = () => {
+
+  const [topCharacters, setTopCharacters] = useState<string[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function downloadFilmData() {
+    setIsLoading(true);
+
+    let finishedDownloading = false;
+    let filmPageData = [];
+
+    //fetching the data from every page
+    for (let i=1; finishedDownloading=true ; i++) {
+      const response = await api.get('/films/?page=' + i);
+      filmPageData.push(response.data.results);
+
+      if(response.data.next === null){
+        finishedDownloading = true;
+        break;
+      }
+    }
+    
+    //Joining all the data in one single array
+    const filmArray = filmPageData.flat();
+    console.log(filmArray)
+    const allCharacters: Array<string> = filmArray.map((arr)=>arr.characters).flat();
+    
+    // console.log(peopleArray);
+    console.log(allCharacters.flat());
+
+    var m = allCharacters.reduce((a:any, b:any) => {
+      a[b] = ++a[b] || 1;
+      return a;
+    }, {});
+
+    var arr = [];
+    for (var key in m) {
+      arr.push([key, m[key]]);
+    }
+
+    arr.sort((a, b) => {
+      return a[1] - b[1];
+    });
+
+    const top5PeopleUrls: Array<string> = arr.slice(-5).map((e) => e[0]);
+    fetchPeopleUrls(top5PeopleUrls);
+  }
+  
+  async function fetchPeopleUrls(peopleURL: Array<string>){
+
+    const apiURLs: Array<string> = peopleURL;
+    let top5characters: Array<string> = [];
+
+    for (let i=0; i<=4; i++) {
+      const response = await api.get(apiURLs[i]);
+      top5characters.push(response.data.name);
+    }
+
+    setTopCharacters(top5characters);
+    setIsLoading(false);
+  }
+  
+  useEffect(()=>{
+    downloadFilmData();
+  },[])
+
   return (
     <div>
         <strong>MOST POPULAR CHARACTERS</strong>
-        <h4>STARSHIP</h4>
-        <h4>STARSHIP</h4>
-        <h4>STARSHIP</h4>
-        <h4>STARSHIP</h4>
-        <h4>STARSHIP</h4>
+        {isLoading
+        ?(
+          <h4>Loading...</h4>
+        ):(
+          topCharacters.map((characterName)=>{
+            return <h4 key={characterName}>{characterName}</h4>
+          })
+        )}
     </div>
   );
 }
