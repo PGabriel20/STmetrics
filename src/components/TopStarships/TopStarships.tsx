de26e8ae75874b36a75a908f96ede20c4fc9ef1f
PGabriel20 <bielporreca@gmail.com>
2021-08-14 15:43:13 -0300

    feat: fetching and sorting top 5 starships

--- src/components/TopStarships/TopStarships.tsx
+++ src/components/TopStarships/TopStarships.tsx
@@ -1,16 +1,82 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
 const TopStarships: React.FC = () => {
+
+  const [topStarships, setTopStarships] = useState<string[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function downloadPeopleData() {
+    setIsLoading(true);
+
+    let finishedDownloading = false;
+    let peoplePageData = [];
+
+    //fetching the data from every page
+    for (let i=1; finishedDownloading=true ; i++) {
+      const response = await api.get('/people/?page=' + i);
+      peoplePageData.push(response.data.results);
+
+      if(response.data.next === null){
+        finishedDownloading = true;
+        break;
+      }
+    }
+    
+    //Joining all the data in one single array
+    const peopleArray = peoplePageData.flat();
+    const allStarships: Array<string> = peopleArray.map((arr)=>arr.starships).flat();
+    
+    var m = allStarships.reduce((a:any, b:any) => {
+      a[b] = ++a[b] || 1;
+      return a;
+    }, {});
+
+    var arr = [];
+    for (var key in m) {
+      arr.push([key, m[key]]);
+    }
+
+    arr.sort((a, b) => {
+      return a[1] - b[1];
+    });
+
+    const top5StarshipsUrls: Array<string> = arr.slice(-5).map((e) => e[0]);
+
+    fetchPeopleUrls(top5StarshipsUrls);
+  }
+  
+  async function fetchPeopleUrls(starshipsURL: Array<string>){
+
+    const apiURLs: Array<string> = starshipsURL;
+    let top5starships: Array<string> = [];
+
+    for (let i=0; i<=4; i++) {
+      const response = await api.get(apiURLs[i]);
+      top5starships.push(response.data.name);
+    }
+
+    setTopStarships(top5starships);
+    setIsLoading(false);
+  }
+  
+  useEffect(()=>{
+    downloadPeopleData();
+  },[])
+
   return (
     <div>
-        <strong>MOST POPULAR STARSHIP</strong>
-        <h4>STARSHIP</h4>
-        <h4>STARSHIP</h4>
-        <h4>STARSHIP</h4>
-        <h4>STARSHIP</h4>
-        <h4>STARSHIP</h4>
+        <strong>MOST POPULAR STARSHIPS</strong>
+        {isLoading
+        ?(
+          <h4>Loading...</h4>
+        ):(
+          topStarships.map((starshipName)=>{
+            return <h4>{starshipName}</h4>
+          })
+        )}
     </div>
   );
 }
